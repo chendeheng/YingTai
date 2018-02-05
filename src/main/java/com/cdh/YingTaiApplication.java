e3bf44140cf607bc8cfdb26267302e6bb958ed3c
chendeheng <chendeheng@chendehengdeMacBook-Pro.local>
2018-02-05 17:24:24 +0800

    init

---
+++ src/main/java/com/cdh/YingTaiApplication.java
@@ -0,0 +1,16 @@
+package com.cdh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chendeheng   上午11:51 2018/2/5
+ */
+@SpringBootApplication
+public class YingTaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YingTaiApplication.class, args);
+    }
+
+}
